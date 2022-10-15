12ffcb500c867ae91a7b2f86be7228dde89f5d22
Gabriel Costa Sampaio <102552939+DevSampaioGabriel@users.noreply.github.com>
2022-10-15 11:57:29 -0300

    Add files via upload

---
+++ Cópia de um site/Site cópia/index.js
@@ -0,0 +1,60 @@
+/*
+'let' é usado para declarar a variável no JS:
+let x = 1;
+'const' é uma variável constante no Js:
+const y = 1;
+'console.log' mostrará no console do navegador:
+console.log(x + y)*/
+/*
+para fazer a condição se no JS utiliza-se:
+if (1 <2) {
+    console.log
+}
+fazer a condição senão se no JS utiliza-se:
+else if (2 == 2) {
+
+}
+fazer a condição senão no Js utiliza-se:
+else {
+
+}
+*/
+/*comparador 'e' e 'ou' no Js:
+'||' comparador 'ou'
+'&&' comparador 'e'
+*/
+/* enquanto no JS:
+while (condição) {
+    console.log
+}
+*/
+/* percorrer com JS:
+for (let  = 0;  < 10; index++) {
+    console.log 
+}
+*/
+/* Definir uma função no JS:
+function teste ()
+
+{
+*/
+/* Lista em JS:
+let lista = [1,2,3,4,5]
+*/
+/* Função anônima no JS:
+let funcao = () => {
+
+}
+*/
+/* Chamar a função no Js:
+teste()
+*/
+/* Utilizar o for para percorrer a lista no JS:
+for (let index = 0; index < lista.lenght ; index++) {
+    console.log(lista[index])
+}
+/*
+/* Percorrendo com outro método:
+lista.forEach((num)=>{
+    console.log(num)
+})
